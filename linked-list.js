b25c8bacc77aeb9976906ebf5d2162e572aeff8f
Christopher Alley <calleyviola@gmail.com>
2023-11-06 15:36:22 -0500

    Add setAt and insertAt

--- linked-list.js
+++ linked-list.js
@@ -93,7 +93,7 @@ class LinkedList {
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-    if (idx >= this.length) throw new Error("Index is invalid");
+    if (idx >= this.length || idx < 0) throw new Error("Index is invalid");
 
     // let current = this.head;
     // for (let i = 0; i < this.length; i++) {
@@ -122,14 +122,60 @@ class LinkedList {
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
+    if (idx >= this.length || idx < 0) throw new Error("Invalid index");
 
+    let current = this.head;
+    let i = 0;
+
+    while (i !== idx) {
+      i++;
+      current = current.next;
+    }
+
+    current.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    if (idx > this.length || idx < 0) throw new Error("invalid index");
 
+    if (this.length === 0) {
+      this.push(val);
+      return;
+    }
+
+    if (idx === 0) {
+      this.unshift(val);
+      return;
+    }
+
+    const newNode = new Node(val);
+
+    let current = this.head;
+    let i = 0;
+
+    while (i < this.length) {
+      if (i === idx - 1) {
+        let temp = current.next;
+        current.next = newNode;
+        current = temp;
+        i++;
+      } else if (i === idx) {
+        newNode.next = current;
+        break;
+      } else {
+        i++;
+        current = current.next;
+      }
+    }
+    this.length += 1;
   }
+
+  // [dog, cat, puppy] insert("frog", 2) => [dog, cat, puppy]
+  // node(idx - 1) -> newNode -> node(idx)
+  // node(idx - 1).next = newNode
+  // node(idx) newNode.next = node(idx)
 
   /** removeAt(idx): return & remove item at idx, */
 
