801e36f92b201b695779d915f2b337fbaeea85cf
Christopher Alley <calleyviola@gmail.com>
2023-11-06 14:49:04 -0500

    Add push, unshift, pop

--- linked-list.js
+++ linked-list.js
@@ -23,19 +23,49 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (this.head === null) this.head = newNode;
+    if (this.tail !== null) this.tail.next = newNode;
+    this.tail = newNode;
+    this.length += 1;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    const newNode = new Node(val);
+    if (this.head !== null) newNode.next = this.head;
+    if (this.tail === null) this.tail = newNode;
+    this.head = newNode;
+    this.length += 1;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    // Handle empty list
+    if (this.tail === null) throw new Error("List is empty");
 
+    // Handle list length 1
+    if (this.length === 1) {
+      const popped = this.tail.val
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return popped;
+    }
+
+    // Handle length > 1
+    let current = this.head;
+    while (current.next !== null) {
+      if (current.next === this.tail) {
+        const popped = this.tail.val
+        this.tail = current;
+        this.length -= 1;
+        return popped;
+      }
+      current = current.next;
+    }
   }
 
   /** shift(): return & remove first item. */
@@ -71,7 +101,7 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
 
