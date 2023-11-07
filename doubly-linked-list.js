9f85d40af60baeef06fb63a6a0732bc00e1bb93f
Christopher Alley <calleyviola@gmail.com>
2023-11-06 20:02:46 -0500

    Add push for dll

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,7 +1,16 @@
 /** Node: node for a doubly linked list. */
 
 class Node {
-  
+  val = null;
+  next = null;
+  prev = null;
+
+  constructor(val, prev=null, next=null) {
+    this.val = val;
+    this.prev = prev;
+    this.next = next;
+  }
+
 }
 
 class DoublyLinkedList {
@@ -16,19 +25,36 @@ class DoublyLinkedList {
   /** get(idx) returns a node at the given index */
 
   _get(idx) {
-    
+    if (idx >= this.length || idx < 0) throw new Error("Index is invalid");
+
+    let i = 0;
+    let current = this.head;
+
+    while (i < this.length) {
+      if (i === idx) return current.val;
+
+      current = current.next;
+      i++;
+    }
   }
 
   /** push(val): add new value to end of list. */
 
   push(val) {
-    
+    const newNode = new Node(val, this.tail, null);
+    if (this.head === null) this.head = newNode;
+    if (this.tail !== null) this.tail.next = newNode;
+    this.tail = newNode;
+    this.length += 1;
   }
 
   /** unshift(val): add new value to start of list. */
 
-  unshift(val) {
-    
+  unshift(val) { //TODO: do the hookups
+    const newNode = new Node(val, null, this.head);
+    if (this)
+    this.head = newNode;
+    this.length += 1;
   }
 
   /** pop(): remove last item & return its value */
@@ -40,37 +66,37 @@ class DoublyLinkedList {
   /** shift(): remove first item & return its value */
 
   shift() {
-    
+
   }
 
   /** getAt(idx): get val at idx.*/
 
   getAt(idx) {
-   
+
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-   
+
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
-   
+
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-   
+
   }
 
   /** return average (mean) of list values. */
 
   average() {
-   
+
   }
 }
 
